d2e29060306eb8ce5704c8910b8404b1764b9b98
brilu-22 <231214@virtualwindow.co.za>
2024-06-01 22:31:27 +0200

    commit

--- api/app.js
+++ api/app.js
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
